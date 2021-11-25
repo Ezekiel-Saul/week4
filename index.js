60d92631601d037b9d8c77024f33278d3c475eea
Ezekiel-Saul <79631754+Ezekiel-Saul@users.noreply.github.com>
2021-11-25 20:25:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-
+import http from 'http';
+var port = process.env.PORT || 4321;
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+  };
+const server = http.createServer(async(req,res)=>{
+      if(req.url === '/result4/'){
+          
+          res.writeHead(200,{
+              'Content-Type':'application/json',
+              ...CORS,
+          })
+          let data = '';
+          await req.on('data', function(chunk){
+            data += chunk;
+            }).on('end', () => {
+          })
+          res.write(JSON.stringify({
+            "message":"itmo247526",
+            "x-result":headers,
+            "x-body":data
+            }
+            ))
+      }
+      res.end()
+      
+  });
+  server.listen(port,()=>{
+      console.log('Server is running')});
